bcf3c1da929def8fa081f71c6b2a8ea56984b2f3
YaroslavGitHub <yaroslav.ivanenko@gmail.com>
2019-03-30 10:13:45 +0200

    start working

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+/*** Don't touch this code! ***/
+const generate_price = () => (Math.floor(Math.random() * 900) + 100) + " $";
 
+let phones = [{
+        title: "iPhone 8",
+        price: generate_price()
+    },
+    {
+        title: "iPhone 7",
+        price: generate_price()
+    },
+    {
+        title: "Apple iPhone 6",
+        price: generate_price()
+    },
+    {
+        title: "Samsung Galaxy S9",
+        price: generate_price()
+    },
+    {
+        title: "Nokia 3310",
+        price: generate_price()
+    },
+    {
+        title: "HTC Two M3",
+        price: generate_price()
+    },
+    {
+        title: "Lenovo H6000",
+        price: generate_price()
+    },
+    {
+        title: "Microsoft Lumia 950",
+        price: generate_price()
+    },
+    {
+        title: "Sony Ericsson K750 Gold",
+        price: generate_price()
+    }
+];
+
+console.dir(phones);
+/***Write code after this line***/
